ee6a0cd836c4114bffa5c1f3fc1551b76e973a2d
hoduy2904 <hoduy2904@gmail.com>
2023-07-22 21:49:37 +0700

    fix css category-product

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   $(".banner").slick({
     infinite: true,
     speed: 500,
@@ -7,7 +7,7 @@ $(function(){
     prevArrow: `<div class="prev"><i class="icon arrow-left-icon"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/></svg></i></div>`,
     nextArrow: `<div class="next"><i class="icon arrow-right-icon"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512"><path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg></i></div>`,
   });
-  
+
   $(".slide-lazy").slick({
     lazyLoad: "ondemand",
     slidesToShow: 5,
@@ -33,7 +33,7 @@ $(function(){
       },
     ],
   });
-  
+
   $(".flash-sale__products").slick({
     lazyLoad: "ondemand",
     slidesToShow: 5,
@@ -59,19 +59,19 @@ $(function(){
       },
     ],
   });
-  
+
   $(".nav-item .icon").on("click", function (e) {
     $(this).parent().toggleClass("active");
     $(this).parent().children(".dropdown-menu").slideToggle();
   });
-  
+
   $(".mobile-menu").on("click", function () {
     $(".sidebar-mobile").addClass("show");
   });
   $(".close-menu").on("click", function () {
     $(".sidebar-mobile").removeClass("show");
   });
-  
+
   $(".image-product").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -87,25 +87,27 @@ $(function(){
     centerMode: false,
     focusOnSelect: true,
   });
-  
+
   const options = {
     root: null,
-    rootMargin: '0px',
+    rootMargin: "0px",
     threshold: 0,
-  }
-  let obcallBack = (entries, observer) => { 
-    entries.forEach(entry => {
-    if(entry.isIntersecting){
+  };
+  let obcallBack = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
         entry.target.src = entry.target.dataset.src;
         observer.unobserve(entry.target);
-    }
+      }
     });
   };
-  
-  const observer = new IntersectionObserver(obcallBack, options)
-  
-  document.querySelectorAll('.lazy-img').forEach(img => { observer.observe(img) });
-  
+
+  const observer = new IntersectionObserver(obcallBack, options);
+
+  document.querySelectorAll(".lazy-img").forEach((img) => {
+    observer.observe(img);
+  });
+
   $(".tab-item").on("click", function () {
     $(".tab-item").removeClass("active");
     $(this).addClass("active");
@@ -114,82 +116,101 @@ $(function(){
     $(`#${currentTab}`).addClass("active");
   });
 
-      $("#btnTracking").on("click", function () {
-        $(".tracking-result").slideToggle();
-      });
-      
-      $(".form-check-input").on("click", function () {
-        console.log($(this));
-        if ($(this).is(":checked")) {
-          $(".form-VAT").show("slide");
+  $("#btnTracking").on("click", function () {
+    $(".tracking-result").slideToggle();
+  });
+
+  $(".form-check-input").on("click", function () {
+    console.log($(this));
+    if ($(this).is(":checked")) {
+      $(".form-VAT").show("slide");
+    } else {
+      $(".form-VAT").hide("slide");
+    }
+  });
+
+  $(".add-to-cart").on("click", function (e) {
+    e.preventDefault();
+    let id = $(this).attr("data-id");
+    let optionPrice = $("input[name=option-price]:checked").val() ?? 1;
+    let color = $("input[name=color]:checked").attr("id");
+    addCart(id, optionPrice, color);
+  });
+
+  $(".buy-now").on("click", function (e) {
+    e.preventDefault();
+  });
+
+  let addCart = function (id, number = 1, colorId = null) {
+    $.ajax({
+      url: "/api/cart/add",
+      method: "POST",
+      data: {
+        id,
+        number,
+        colorId,
+      },
+      success: function (data) {
+        if (data.status == 200) {
+          showToast("Thêm giỏ hàng thành công");
+          $("#cartCount").html(data.count);
         } else {
-          $(".form-VAT").hide("slide");
+          showToast(data.message);
         }
-      });
+      },
+      error: function (xhr, status, error) {
+        var err = eval("(" + xhr.responseText + ")");
+        showToast(err.message, "error");
+      },
+    });
+  };
 
-      $('.add-to-cart').on('click',function(e){
-        e.preventDefault();
-        let id = $(this).attr('data-id');
-        let optionPrice = $('input[name=option-price]:checked').val()??1;
-        let color = $('input[name=color]:checked').attr('id');
-        addCart(id,optionPrice,color);
-      })
-
-      $('.buy-now').on('click',function(e){
-        e.preventDefault();
-      })
-
-      let addCart = function(id, number = 1, colorId=null) {
-        $.ajax({
-            url: '/api/cart/add',
-            method: "POST",
-            data: {
-                id,
-                number,
-                colorId
-            },
-            success: function(data) {
-                if (data.status == 200) {
-                    showToast("Thêm giỏ hàng thành công");
-                    $('#cartCount').html(data.count);
-                }else{
-                    showToast(data.message);
-                }
-            },
-            error: function(xhr, status, error) {
-              var err = eval("(" + xhr.responseText + ")");
-              showToast(err.message,"error");
-            }
-        })
+  function showToast(text, type) {
+    let className = "toast-default";
+    switch (type) {
+      case "error": {
+        className = "toast-error";
+        break;
+      }
+      case "success": {
+        className = "toast-success";
+        break;
+      }
+      case "warning": {
+        className = "toast-warning";
+        break;
+      }
+      default:
+        break;
     }
 
-      function showToast(text, type){
-        let className = 'toast-default';
-        switch(type){
-          case "error":{
-            className = 'toast-error';
-            break;
-          }
-          case "success":{
-            className = 'toast-success';
-            break;
-          }
-          case "warning":{
-            className='toast-warning'
-            break;
-          }
-          default: break;
-        }
+    Toastify({
+      text: text,
+      className: className,
+      close: true,
+      offset: {
+        x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+        y: 10, // vertical axis - can be a number or a string indicating unity. eg: '2em'
+      },
+    }).showToast();
+  }
 
-        Toastify({
-          text: text,
-          className: className,
-          close: true,
-          offset: {
-            x: 50, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: 10 // vertical axis - can be a number or a string indicating unity. eg: '2em'
-          },
-        }).showToast();
-      }
-      
+  $(".filter-item").on("click", function () {
+    if ($(this).children(".filter-content").is(":hidden")) {
+      $(this).children(".category-title").children(".end-icon").html("-");
+      console.log($(this).children(".category-title").children(".end-icon"));
+    } else {
+      $(this).children(".category-title").children(".end-icon").html("+");
+    }
+    $(this).children(".filter-content").slideToggle();
   });
+
+  $(".filters-title .xmark-icon").on("click", function () {
+    console.log("hi");
+    $(".filters").removeClass("active");
+  });
+  $("#open-filter").on("click", function (e) {
+    e.preventDefault();
+    $(".filters").toggleClass("active");
+  });
+});
